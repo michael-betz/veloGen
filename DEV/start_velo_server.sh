9246db4923f21ec5822b3b18ce780dab9bf73c99
Michael Betz <michael.betz@terapet.ch>
2022-07-22 11:32:22 +0200

    fix start_velo_server.sh

--- DEV/start_velo_server.sh
+++ DEV/start_velo_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 screen -r velo -X quit
 
-# CMD = 'python3 velo_server.py --topic "velo1/raw,velo2/raw"'
-CMD = 'python3 velo_server2.py -t velo1/raw'
+# CMD='./velo_server.py --topic "velo1/raw,velo2/raw"'
+CMD='./velo_server2.py -t velo1/raw'
 
-screen -dmS velo -L velo_log.txt bash -c $CMD
+screen -dmLS velo -Logfile velo_log.txt $CMD
