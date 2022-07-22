50fc54467f7354bcb799d7e82e330c3938c55565
Michael Betz <michael.betz@terapet.ch>
2022-07-22 19:21:07 +0200

    fix screen logging

--- DEV/start_velo_server.sh
+++ DEV/start_velo_server.sh
@@ -4,4 +4,4 @@ screen -r velo -X quit
 # CMD='./velo_server.py --topic "velo1/raw,velo2/raw"'
 CMD='./velo_server2.py -t velo1/raw'
 
-screen -dmLS velo -Logfile velo_log.txt $CMD
+screen -dmS velo -L -Logfile velo_log.txt $CMD
